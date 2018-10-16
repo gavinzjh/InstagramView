782e569c5f29e0fd9dd5c05eed37ec1ce2e0ec4e
gavinzjh <gavinzhujh@gmail.com>
2018-10-17 04:49:28 +1100

    Profile Page

---
+++ InstagramView/View/ProfileHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileHeaderView.swift
+//  InstagramView
+//
+//  Created by ZJH on 2018/10/17.
+//  Copyright © 2018年 ZJH. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderView: UICollectionReusableView {
+    @IBOutlet weak var profilePhoto: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var posts: UILabel!
+    @IBOutlet weak var followers: UILabel!
+    @IBOutlet weak var followings: UILabel!
+    @IBOutlet weak var postsTitle: UILabel!
+    @IBOutlet weak var followersTitle: UILabel!
+    @IBOutlet weak var followingsTitle: UILabel!
+    
+}
---
+++ InstagramView/View/ProfilePictureCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProfilePictureCell.swift
+//  InstagramView
+//
+//  Created by ZJH on 2018/10/17.
+//  Copyright © 2018年 ZJH. All rights reserved.
+//
+
+import UIKit
+
+class ProfilePictureCell: UICollectionViewCell {
+    @IBOutlet weak var pictureImage: UIImageView!
+    
+}
